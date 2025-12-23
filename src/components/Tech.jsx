c352d83c6e67e1c5f4502fd54cf4795b177d016a
vignesh <vignesh7624989827@gmail.com>
2025-12-23 16:41:42 +0530

    Add heading and description to tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,13 +6,28 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className='text-center mb-12'>
+        <p className='text-secondary text-[18px] uppercase tracking-wider mb-4'>
+          My Technical Arsenal
+        </p>
+        <h2 className='text-white text-[40px] md:text-[50px] font-bold mb-6'>
+          Technologies I'm Familiar With
+        </h2>
+        <p className='text-secondary text-[16px] max-w-3xl mx-auto leading-relaxed'>
+          Here are the technologies and tools I work with to build modern, scalable applications.
+          I'm continuously learning and expanding my skill set to stay current with industry trends.
+        </p>
+      </div>
+
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
